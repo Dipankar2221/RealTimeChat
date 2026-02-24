620782c7e1e273213bb6e0d8ae84feb6fbe46a61
Dipankar Mandal <mandaldipankar2004@gmail.com>
2026-02-24 11:54:43 +0530

    fix: update CORS origin and improve static file serving

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
@@ -18,9 +16,10 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://realtimechat-q6sq.onrender.com",
     credentials: true,
   })
 );
@@ -31,12 +30,14 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  app.use((req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../frontend/dist/index.html")
+    );
   });
 }
 
 server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
+  console.log("Server running on PORT:", PORT);
   connectDB();
 });
